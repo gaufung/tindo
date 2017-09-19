8f05f36c952e5ae2d41c85379d3fb67d6001d128
gaufung <gaufung@outlook.com>
2017-09-19 17:22:41 +0800

    add guess number game

--- example/urls.py
+++ example/urls.py
@@ -2,14 +2,16 @@
 import sys
 sys.path.insert(0, '../')
 reload(sys)
+import random
 from tindo.tindo import get, view, post, ctx, route
-
+from tindo.http import redirect
 
 @view('index.html')
 @route('/')
 def index():
     session = ctx.response.session
-    session.name = 'gaofeng'
+    session.number = random.randint(1, 100)
+    session.times = 10
     return dict()
 
 
@@ -42,6 +44,31 @@ def comment(name, group):
 def session():
     sess = ctx.response.session
     return dict(name=sess.name)
+
+
+@view('game.html')
+@route('/game', methods=['GET', 'POST'])
+def game():
+    i = ctx.request.input(number=None)
+    times = ctx.response.session.times
+    number = ctx.response.session.number
+    guess_number = i.get('number')
+    if guess_number is not None:
+        guess_number = int(guess_number)
+        if times:
+            times -= 1
+            ctx.response.session.times = times
+            if guess_number > number:
+                return dict(success=False, status='Too Big', times=times)
+            elif guess_number < number:
+                return dict(success=False, status='Too Small', times=times)
+            else:
+                return dict(success=True)
+        else:
+            redirect('/')
+    else:
+        return dict(times=times, status='')
+
 
 
 
